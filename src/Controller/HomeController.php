7f2e34b067ad61f8980bced6b9c5559677709e2a
Ed <edouard.duron@lapiscine.pro>
2024-11-25 11:43:36 +0100

    Creating HomeController

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+// Creating new class: HomeController
+class HomeController{
+
+    // Adding the access, '/' become the function above
+    #[Route('/', 'home')]
+
+    //I create the method "home"
+
+public function home(){
+
+        // This method return a new Response
+
+        return new Response('<h1>Page Accueil</h1>');
+    }
+}
