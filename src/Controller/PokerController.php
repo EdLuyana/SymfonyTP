168e35f7f35e819c8df86d0c6b0a5a60559f3e7a
Ed <edouard.duron@lapiscine.pro>
2024-11-25 12:43:24 +0100

    Creating PokerController

--- src/Controller/PokerController.php
+++ src/Controller/PokerController.php
@@ -2,28 +2,22 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 // Creating new class Poker
-class Poker{
-
+class PokerController{
     //Giving the URL access to the function above
 #[Route('/poker', name: 'poker')]
-
 //Creating a new method 'poker'
     public function poker(){
-
-
-    // Calling and creating a new createFromGlobals w/o using "new"
+        // Calling and creating a new createFromGlobals w/o using "new"
         // This one fills $request with all datas from GET, POST, SESSION, etc...
         $request = Request::createFromGlobals();
-
         //I use query to get all my datas
         $age = $request->query->get('age');
-
         var_dump($age); die;
-
         return new Response('Bienvenue sur le site de Poker');
     }
 }
