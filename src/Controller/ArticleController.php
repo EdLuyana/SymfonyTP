36c774e0912285cbe6e24710c68c4b3b0023034b
Ed <edouard.duron@lapiscine.pro>
2024-11-25 17:16:11 +0100

    CSS c'est pas des lol

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -16,30 +16,35 @@ class ArticleController extends AbstractController
                 'title' => 'Power Ranger Bleu',
                 'content' => 'Content of article 1',
                 'image' => 'https://static.vecteezy.com/system/resources/thumbnails/012/176/986/small_2x/a-3d-rendering-image-of-grassed-hill-nature-scenery-png.png',
+                'color' => 'blue'
             ],
             [
                 'id' => 2,
                 'title' => 'Power Ranger Rouge',
                 'content' => 'Content of article 2',
                 'image' => 'https://static.vecteezy.com/system/resources/thumbnails/012/176/986/small_2x/a-3d-rendering-image-of-grassed-hill-nature-scenery-png.png',
+                'color' => 'red'
             ],
             [
                 'id' => 3,
                 'title' => 'Power Ranger Jaune',
                 'content' => 'Content of article 3',
                 'image' => 'https://static.vecteezy.com/system/resources/thumbnails/012/176/986/small_2x/a-3d-rendering-image-of-grassed-hill-nature-scenery-png.png',
+                'color' => 'yellow'
             ],
             [
                 'id' => 4,
                 'title' => 'Power Ranger Rose',
                 'content' => 'Content of article 4',
                 'image' => 'https://static.vecteezy.com/system/resources/thumbnails/012/176/986/small_2x/a-3d-rendering-image-of-grassed-hill-nature-scenery-png.png',
+                'color' => 'pink'
             ],
             [
                 'id' => 5,
                 'title' => 'Power Ranger Vert',
                 'content' => 'Content of article 5',
                 'image' => 'https://static.vecteezy.com/system/resources/thumbnails/012/176/986/small_2x/a-3d-rendering-image-of-grassed-hill-nature-scenery-png.png',
+                'color' => 'green'
             ]
 
         ];
