a7e4f30f94406f4356bbf5bb8c756467727d14ab
Ed <edouard.duron@lapiscine.pro>
2024-11-26 10:29:27 +0100

    Article Show

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,11 +3,12 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController extends AbstractController
 {
-#[Route('/article', name: 'articles_list')]
+    #[Route('/articles', name: 'articles_list')]
     public function articles()
     {
         $articles = [
@@ -50,5 +51,66 @@ class ArticleController extends AbstractController
         ];
 
         return $this->render('articles_list.html.twig', ['articles' => $articles]);
+    }
+
+    // Creating the URL for my new page about article
+    #[Route('/article', 'article_show')]
+    public function showArticle()
+    {
+// Using Request class from Symfony thanks to createFromGlobals, this way my $request receive all infos from GET, POST, IP etc...)
+        $request = Request::createFromGlobals();
+        // Here $id will able to take id's from get
+        $id = $request->query->get('id');
+
+        $articles = [
+            [
+                'id' => 1,
+                'title' => 'Power Ranger Bleu',
+                'content' => 'Content of article 1',
+                'image' => 'https://static.vecteezy.com/system/resources/thumbnails/012/176/986/small_2x/a-3d-rendering-image-of-grassed-hill-nature-scenery-png.png',
+                'color' => 'blue'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Power Ranger Rouge',
+                'content' => 'Content of article 2',
+                'image' => 'https://static.vecteezy.com/system/resources/thumbnails/012/176/986/small_2x/a-3d-rendering-image-of-grassed-hill-nature-scenery-png.png',
+                'color' => 'red'
+            ],
+            [
+                'id' => 3,
+                'title' => 'Power Ranger Jaune',
+                'content' => 'Content of article 3',
+                'image' => 'https://static.vecteezy.com/system/resources/thumbnails/012/176/986/small_2x/a-3d-rendering-image-of-grassed-hill-nature-scenery-png.png',
+                'color' => 'yellow'
+            ],
+            [
+                'id' => 4,
+                'title' => 'Power Ranger Rose',
+                'content' => 'Content of article 4',
+                'image' => 'https://static.vecteezy.com/system/resources/thumbnails/012/176/986/small_2x/a-3d-rendering-image-of-grassed-hill-nature-scenery-png.png',
+                'color' => 'pink'
+            ],
+            [
+                'id' => 5,
+                'title' => 'Power Ranger Vert',
+                'content' => 'Content of article 5',
+                'image' => 'https://static.vecteezy.com/system/resources/thumbnails/012/176/986/small_2x/a-3d-rendering-image-of-grassed-hill-nature-scenery-png.png',
+                'color' => 'green'
+            ]
+
+        ];
+// I create my new var $article as null for now
+        $article = null;
+
+        // for each articles in the array I check if his ad match with id from get method, if yes articleFound become the article with the id
+        foreach ($articles as $article) {
+            if ($article['id'] === (int)$id) {
+                $articleFound = $article;
+            }
+        }
+// I do a return an HTTP answer thanks to render method
+        return $this->render('article_show.html.twig',
+            ['article' => $articleFound]);
     }
 }
